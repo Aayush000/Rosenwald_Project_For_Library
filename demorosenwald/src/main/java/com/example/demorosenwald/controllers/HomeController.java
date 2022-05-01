9fb3228ee542e8acaab0a8712b510cd40e6b5142
Lucky Abolorunke <luckyabolorunke@gmail.com>
2022-05-01 17:40:57 -0500

    all schools controller

--- demorosenwald/src/main/java/com/example/demorosenwald/controllers/HomeController.java
+++ demorosenwald/src/main/java/com/example/demorosenwald/controllers/HomeController.java
@@ -3,7 +3,13 @@ package com.example.demorosenwald.controllers;
 import com.example.demorosenwald.service.RosenwaldSchoolDataService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class HomeController {
@@ -19,10 +25,6 @@ public class HomeController {
         model.addAttribute("schools", rosenwaldSchoolDataService.getHomeSchoolData());
         return "index";
     }
-    @RequestMapping("/all_schools")
-    public String all_schools(){
-        return "all_schools";
-    }
 
     @RequestMapping("/about")
     public String about(){
@@ -34,5 +36,91 @@ public class HomeController {
         return "school";
     }
 
+
+    /**
+     *Controller for all school
+     */
+
+//    @RequestMapping("/all_schools")
+//    public String all_schools(){
+//        return "all_schools";
+//    }
+
+
+//    @GetMapping("/all_schools")
+//    public String viewHomePage(Model model) {
+//        return findPaginated(1,model);
+//    }
+//
+//    @GetMapping("/all_schools/{pageNo}")
+//    public String findPaginated(@PathVariable(value = "pageNo") int pageNo,Model model) {
+//        int pageSize = 9;
+//        int totalPage = 6;
+//        System.out.println(pageNo);
+////        Page<SchoolData> page = rosenwaldSchoolDataService.findPaginated(pageNo, pageSize, sortField, sortDir);
+////        List<SchoolData> listEmployees = page.getContent();
+////
+//        model.addAttribute("currentPage", pageNo);
+//        model.addAttribute("totalPages", totalPage);
+//        model.addAttribute("totalItems", pageSize);
+//        //
+//        model.addAttribute("schools", rosenwaldSchoolDataService.getHomeSchoolData());
+//        return "all_schools";
+//    }
+
+
+
+    @RequestMapping(value = "/all_schools", method = RequestMethod.GET)
+    public String allSchools(Model model, @RequestParam("page") Optional<Integer> page) {
+
+
+
+        int currentPage = page.orElse(1);
+        int pageSize = rosenwaldSchoolDataService.getAllSchoolsPage().get(currentPage).size();
+        int totalPages = rosenwaldSchoolDataService.getAllSchoolsPage().size()-1;
+
+//        Page<Book> bookPage = bookService.findPaginated(PageRequest.of(currentPage - 1, pageSize));
+
+        model.addAttribute("schools", rosenwaldSchoolDataService.getAllSchoolsPage().get(currentPage));
+        model.addAttribute("totalPages", totalPages);
+        model.addAttribute("totalItems", pageSize);
+        model.addAttribute("currentPage", currentPage);
+
+
+//        int totalPages = bookPage.getTotalPages();
+        if (totalPages > 0) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
+
+        return "all_schools";
+    }
+
+
+
+//    @RequestMapping(value = "/allSchools", method = RequestMethod.GET)
+//    public String allSchools(
+//            Model model,
+//            @RequestParam("page") Optional<Integer> page,
+//            @RequestParam("size") Optional<Integer> size) {
+//        int currentPage = page.orElse(1);
+//        int pageSize = size.orElse(5);
+//
+//        Page<Book> bookPage = bookService.findPaginated(PageRequest.of(currentPage - 1, pageSize));
+//
+//        model.addAttribute("bookPage", bookPage);
+//
+//        int totalPages = bookPage.getTotalPages();
+//        if (totalPages > 0) {
+//            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+//                    .boxed()
+//                    .collect(Collectors.toList());
+//            model.addAttribute("pageNumbers", pageNumbers);
+//        }
+//
+//        return "allSchools.html";
+//    }
 
 }
--- demorosenwald/src/main/java/com/example/demorosenwald/service/RosenwaldSchoolDataService.java
+++ demorosenwald/src/main/java/com/example/demorosenwald/service/RosenwaldSchoolDataService.java
@@ -6,18 +6,18 @@ import com.example.demorosenwald.util.UniqueSchoolPhoto;
 import lombok.Getter;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.awt.print.Pageable;
 import java.io.*;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Service
 @Getter
@@ -26,6 +26,8 @@ public class RosenwaldSchoolDataService {
 
     private List<SchoolData> allSchoolData = new ArrayList<>();
     private List<SchoolData> homeSchoolData = new ArrayList<>();
+    Hashtable<Integer, List<SchoolData>> allSchoolsPage = new Hashtable();
+    int itemSizePerPage = 180;
 
     private static String SCHOOL_DATA_URL = "https://raw.githubusercontent.com/Aayush000/Rosenwald_Library/main/schools/school_metadata.csv";
 
@@ -33,7 +35,10 @@ public class RosenwaldSchoolDataService {
 //    @Scheduled(cron = "* * 1 * * *") //run once every day
     public void RosenwaldSchoolDataService() throws IOException, InterruptedException {
         List<SchoolData> newSchoolData = new ArrayList<>();
+        List<SchoolData> subSchoolData = new ArrayList<>();
         Set<String> uniquePhotoId = new HashSet<String>();
+        int index = 0;
+        int count = 0;
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(SCHOOL_DATA_URL)).build();
@@ -47,6 +52,14 @@ public class RosenwaldSchoolDataService {
         SchoolData schoolData = new SchoolData();
 
         for (CSVRecord record : records) {
+
+            if (count == itemSizePerPage){
+
+                allSchoolsPage.put(index, subSchoolData);
+                subSchoolData = new ArrayList<>();
+                count = 0;
+                index++;
+            }
 
             if (!uniquePhotoId.contains(record.get("Photo ID #"))) {
                 if (!sublistUID.isEmpty()) {
@@ -78,15 +91,25 @@ public class RosenwaldSchoolDataService {
                 sublistUID.add(record.get("Photo ID #"));
 
                 newSchoolData.add(schoolData);
+                subSchoolData.add(schoolData);
+                count ++;
             } else {
                 sublistUID.add(record.get("Photo ID #"));
             }
+
             uniquePhotoId.add(record.get("Photo ID #"));
 
 
         }
+
+        if(!subSchoolData.isEmpty()){
+            allSchoolsPage.put(index, subSchoolData);
+        }
+
         homeSchoolData = newSchoolData.subList(newSchoolData.size() - 9, newSchoolData.size());
         allSchoolData = newSchoolData;
 
     }
+
+//
 }
